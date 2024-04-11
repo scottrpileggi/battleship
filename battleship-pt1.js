33b6dc12806ce58d0ed5048997ef5a33fc95c059
Scott Pileggi <scottpileggi@Scotts-MacBook-Pro-2.local>
2024-04-11 13:43:19 -0500

    create grid and sort out ship generator

--- battleship-pt1.js
+++ battleship-pt1.js
@@ -1,1 +1,24 @@
-console.log("lfg");
+let rs = require("readline-sync");
+
+function createGrid() {
+  const letters = [..."ABC"];
+  let arr = [];
+
+  for (let char of letters) {
+    for (let i = 1; i <= 3; i++) {
+      arr.push({ row: char, column: i, hasShip: false, hasBeenHit: false });
+    }
+  }
+  return arr;
+}
+let gameGrid = createGrid();
+
+function shipGenerator() {
+  for (let i = 0; i < 2; i++) {
+    randomSpot.hasBeenHit = true;
+  }
+  return gameGrid;
+}
+
+const randomSpot = gameGrid[Math.floor(Math.random() * gameGrid.length)];
+console.log(shipGenerator());
