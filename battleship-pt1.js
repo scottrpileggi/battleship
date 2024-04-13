c75960c30b30e723b8975c154ea4b2954eafa62b
Scott Pileggi <scottpileggi@Scotts-MacBook-Pro-2.local>
2024-04-13 13:24:02 -0500

    Commit prior to changing game functions that take user input, check the input, then check the game status. Will rewriting functions

--- battleship-pt1.js
+++ battleship-pt1.js
@@ -1,5 +1,7 @@
 var rs = require("readline-sync");
 
+rs.keyInPause("Press any key to start the game.");
+
 function createGrid(size = 3) {
   const letters = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
   let arr = [];
@@ -16,11 +18,8 @@ function createGrid(size = 3) {
   return arr;
 }
 
-let emptyGameGrid = createGrid();
-console.log(emptyGameGrid);
-
-console.log("Press any key to start the game.");
-rs.keyInPause();
+let gameGrid = createGrid();
+console.log(gameGrid);
 
 function selectShipSpots(array, numShips) {
   let shipSpots = array
@@ -30,20 +29,67 @@ function selectShipSpots(array, numShips) {
 }
 
 function placeSmallShips(numShips) {
-  const shipSpots = selectShipSpots(emptyGameGrid, numShips);
+  const shipSpots = selectShipSpots(gameGrid, numShips);
 
-  for (let spot of emptyGameGrid) {
+  for (let spot of gameGrid) {
     if (shipSpots.includes(spot)) {
       spot.hasShip = true;
     }
   }
-  return emptyGameGrid;
+  return gameGrid;
 }
 
-let gameReadyGrid = placeSmallShips(2);
+placeSmallShips(2);
 
-console.log(gameReadyGrid);
+let shipsLeft = 2;
+
+console.log(gameGrid);
 
 function getInput() {
   let userInput = rs.question("Enter a location to strike ie 'A2' ");
+  return userInput;
 }
+
+getInput();
+
+for (let spot of gameGrid) {
+  if (getInput() === `${spot.row}${spot.column}`) {
+    if (spot.hasShip === true) {
+      spot.hasBeenHit === true;
+      spot.hasShip === false;
+      shipsLeft--;
+      console.log(
+        `"Hit. You have sunk a battleship. ${shipsLeft} ships remaining."`
+      );
+      return gameChecker();
+    } else if (spot.hasBeenHit === true) {
+      console.log("You have already picked this location. Miss!");
+      return getInput();
+    } else {
+      console.log("You have missed!");
+      return getInput();
+    }
+  }
+  return getInput();
+}
+
+getInput();
+
+function gameChecker() {
+  if (shipsLeft > 0) {
+    return getInput();
+  } else {
+    let input = rs.keyInYN(
+      "You have destroyed all battleships. Would you like to play again? Y/N"
+    );
+    if (input) {
+      return "Game done.";
+    } else {
+      return createGrid();
+    }
+  }
+}
+
+getInput();
+
+// console.log(getInput());
