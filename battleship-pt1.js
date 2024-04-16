ddb6fc7ba690900dad64de1f0a3e6918570ecc76
Scott Pileggi <scottpileggi@Scotts-MacBook-Pro-2.local>
2024-04-16 15:48:22 -0500

    Complete part 1 of battleship

--- battleship-pt1.js
+++ battleship-pt1.js
@@ -1,95 +1,82 @@
 var rs = require("readline-sync");
 
-rs.keyInPause("Press any key to start the game.");
+const battleShip = () => {
+  rs.keyInPause("Press any key to start the game.");
 
-function createGrid(size = 3) {
-  const letters = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-  let arr = [];
-  for (let i = 0; i < size; i++) {
-    for (let j = 1; j <= size; j++) {
-      arr.push({
-        row: letters[i],
-        column: j,
-        hasShip: false,
-        hasBeenHit: false,
-      });
+  const createGrid = (size = 3) => {
+    const letters = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+    let arr = [];
+    for (let i = 0; i < size; i++) {
+      for (let j = 1; j <= size; j++) {
+        arr.push({
+          row: letters[i],
+          column: j,
+          hasShip: false,
+          hasBeenHit: false,
+        });
+      }
+    }
+    return arr;
+  };
+
+  let gameGrid = createGrid();
+
+  function selectShipSpots(array, numShips) {
+    let shipSpots = array
+      .sort(() => Math.random() - Math.random())
+      .slice(0, numShips);
+    return shipSpots;
+  }
+
+  function placeSmallShips(numShips) {
+    const shipSpots = selectShipSpots(gameGrid, numShips);
+
+    for (let spot of gameGrid) {
+      if (shipSpots.includes(spot)) {
+        spot.hasShip = true;
+      }
+    }
+    return gameGrid;
+  }
+
+  placeSmallShips(2);
+
+  let shipsLeft = 2;
+
+  console.log(gameGrid);
+
+  while (shipsLeft > 0) {
+    function getInput() {
+      let userInput = rs.question("Enter a location to strike ie 'A2' ");
+      return userInput;
+    }
+    let userInput = getInput();
+
+    for (let spot of gameGrid) {
+      if (userInput === `${spot.row}${spot.column}`) {
+        if (spot.hasShip === true) {
+          spot.hasShip = false;
+          spot.hasBeenHit = true;
+          shipsLeft -= 1;
+          console.log(gameGrid);
+          console.log(
+            `Hit! You have sunk a battleship. ${shipsLeft} ship remaining.`
+          );
+        } else if (spot.hasBeenHit === false) {
+          console.log("Miss!");
+          spot.hasBeenHit = true;
+        } else {
+          console.log("Miss! You've already hit this target. try again...");
+        }
+      }
     }
   }
-  return arr;
-}
 
-let gameGrid = createGrid();
-console.log(gameGrid);
+  let playAgain = rs.keyInYN(
+    "You have destroyed all battleships. Would you like to play again? Y/N"
+  );
 
-function selectShipSpots(array, numShips) {
-  let shipSpots = array
-    .sort(() => Math.random() - Math.random())
-    .slice(0, numShips);
-  return shipSpots;
-}
+  playAgain ? battleShip() : console.log("Thanks for playing!");
+};
 
-function placeSmallShips(numShips) {
-  const shipSpots = selectShipSpots(gameGrid, numShips);
-
-  for (let spot of gameGrid) {
-    if (shipSpots.includes(spot)) {
-      spot.hasShip = true;
-    }
-  }
-  return gameGrid;
-}
-
-placeSmallShips(2);
-
-let shipsLeft = 2;
-
-console.log(gameGrid);
-
-function getInput() {
-  let userInput = rs.question("Enter a location to strike ie 'A2' ");
-  return userInput;
-}
-
-getInput();
-
-for (let spot of gameGrid) {
-  if (getInput() === `${spot.row}${spot.column}`) {
-    if (spot.hasShip === true) {
-      spot.hasBeenHit === true;
-      spot.hasShip === false;
-      shipsLeft--;
-      console.log(
-        `"Hit. You have sunk a battleship. ${shipsLeft} ships remaining."`
-      );
-      return gameChecker();
-    } else if (spot.hasBeenHit === true) {
-      console.log("You have already picked this location. Miss!");
-      return getInput();
-    } else {
-      console.log("You have missed!");
-      return getInput();
-    }
-  }
-  return getInput();
-}
-
-getInput();
-
-function gameChecker() {
-  if (shipsLeft > 0) {
-    return getInput();
-  } else {
-    let input = rs.keyInYN(
-      "You have destroyed all battleships. Would you like to play again? Y/N"
-    );
-    if (input) {
-      return "Game done.";
-    } else {
-      return createGrid();
-    }
-  }
-}
-
-getInput();
-
-// console.log(getInput());
+battleShip();
