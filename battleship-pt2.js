27ac7e73a7cadb2e06b80b69aff909739caeeac5
Scott Pileggi <scottpileggi@Scotts-MacBook-Pro-2.local>
2024-04-23 11:51:51 -0500

    Just started shipPlacer function

--- battleship-pt2.js
+++ battleship-pt2.js
@@ -21,16 +21,21 @@ const battleShip = () => {
 
   let gameGrid = createGrid();
 
-  const shipGenerator = (str, size) => {
-    return [{ name: `${str}`, size: size, spotsOccupied: [] }];
+  const shipGenerator = (id, str, size) => {
+    return { id: id, name: `${str}`, size: size, spotsOccupied: [] };
   };
 
+  let ships = [
+    shipGenerator(1, "small", 2),
+    shipGenerator(2, "medium-1", 3),
+    shipGenerator(3, "medium-2", 3),
+    shipGenerator(4, "large", 4),
+    shipGenerator(5, "jumbo", 5),
+  ];
+
   let shipsRemaining = 5;
-  let ship1 = shipGenerator("small", 2);
-  let ship2 = shipGenerator("medium-1", 3);
-  let ship3 = shipGenerator("medium-2", 3);
-  let ship4 = shipGenerator("large", 4);
-  let ship5 = shipGenerator("jumbo", 5);
+
+  console.log(ships);
 
   function randomSelector(array) {
     let randomElem = array
@@ -44,6 +49,13 @@ const battleShip = () => {
     let randomDirection = randomSelector(directionArray);
     return randomDirection;
   };
+
+  const shipPlacer = (ship) => {
+    let startingPoint = randomSelector(gameGrid);
+    if (startingPoint.hasShip)
+  };
+
+  shipPlacer();
 };
 
 battleShip();
