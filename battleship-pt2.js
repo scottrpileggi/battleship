dc44f296e971bbed78ad1bece7f512d7ecdc5e3e
Scott Pileggi <scottpileggi@Scotts-MacBook-Pro-2.local>
2024-04-26 19:00:47 -0500

    ship placement function is working with the 'up' direction! do the 'down direction next!'

--- battleship-pt2.js
+++ battleship-pt2.js
@@ -3,15 +3,16 @@ var rs = require("readline-sync");
 const battleShip = () => {
   rs.keyInPause("Press any key to start the game.");
 
+  const letters = [..."ABCDEFGHIJ"];
+
   const createGrid = (size = 10) => {
-    const letters = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
     let arr = [];
     for (let i = 0; i < size; i++) {
       for (let j = 1; j <= size; j++) {
         arr.push({
           row: letters[i],
           column: j,
-          hasShip: false,
+          hasShip: null,
           hasBeenHit: false,
         });
       }
@@ -20,42 +21,77 @@ const battleShip = () => {
   };
 
   let gameGrid = createGrid();
-
-  const shipGenerator = (id, str, size) => {
-    return { id: id, name: `${str}`, size: size, spotsOccupied: [] };
-  };
-
-  let ships = [
-    shipGenerator(1, "small", 2),
-    shipGenerator(2, "medium-1", 3),
-    shipGenerator(3, "medium-2", 3),
-    shipGenerator(4, "large", 4),
-    shipGenerator(5, "jumbo", 5),
-  ];
-
   let shipsRemaining = 5;
 
-  console.log(ships);
+  console.log(gameGrid);
+
+  let ships = [
+    { name: "small", numOfUnits: 2, spaces: [] },
+    { name: "medium", numOfUnits: 3, spaces: [] },
+    { name: "medium", numOfUnits: 3, spaces: [] },
+    { name: "large", numOfUnits: 4, spaces: [] },
+    { name: "jumbo", numOfUnits: 5, spaces: [] },
+  ];
 
   function randomSelector(array) {
-    let randomElem = array
-      .sort(() => Math.random() - Math.random())
-      .slice(0, 1);
-    return randomElem;
+    return array.sort(() => Math.random() - Math.random()).slice(0, 1)[0];
   }
 
   const randomDirection = () => {
     let directionArray = ["up", "down", "left", "right"];
-    let randomDirection = randomSelector(directionArray);
-    return randomDirection;
+    return randomSelector(directionArray);
   };
 
   const shipPlacer = (ship) => {
+    const shipSize = ship.numOfUnits;
     let startingPoint = randomSelector(gameGrid);
-    if (startingPoint.hasShip)
+    let selectedSpots = [];
+    let direction = "up";
+    let letterIndex = letters.indexOf(startingPoint.row);
+
+    // If starting space is already occupied by another ship, restart the function and try a different random spot..
+    if (startingPoint.hasShip === true) {
+      return shipPlacer(ship);
+    }
+
+    // Run this code block if the random direction is 'up'...
+    if (direction === "up") {
+      if (shipSize > letterIndex + 1) {
+        return shipPlacer(ship);
+      } else {
+        for (let i = letterIndex; i > letterIndex - shipSize; i--) {
+          selectedSpots.push(`${letters[i]}${startingPoint.column}`);
+          console.log(selectedSpots);
+        }
+      }
+      for (let spot of selectedSpots) {
+        for (let elem of gameGrid) {
+          if (spot === `${elem.row}${elem.column}`) {
+            if (elem.hasShip !== null) {
+              spot = null;
+            }
+
+            if (selectedSpots.includes(null)) {
+              return shipPlacer(ship);
+            } else {
+              for (let spot of selectedSpots) {
+                for (let elem of gameGrid) {
+                  if (spot === `${elem.row}${elem.column}`) {
+                    elem.hasShip = ship.name;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      ship.spaces = selectedSpots;
+      return "done";
+    }
   };
-
-  shipPlacer();
+  shipPlacer(ships[4]);
+  shipPlacer(ships[0]);
+  console.log(gameGrid);
+  console.log(ships);
 };
-
 battleShip();
